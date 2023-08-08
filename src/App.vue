bc50339a806acf19beeec345373e7ce46a6b0718
逸yi <497862392@qq.com>
2023-08-08 10:47:40 +0800

    项目初始化-引入vant组件库

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,20 @@
 <template>
   <div id="app">
     <h3>黑马头条</h3>
-   <!-- 路由的出口 -->
+    <div>
+      <i class="toutiao toutiao-shouye"></i>
+    </div>
+    <div>
+      <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+    <van-cell-group inset>
+    <van-cell title="单元格" value="内容" />
+    <van-cell title="单元格" value="内容" label="描述信息" />
+  </van-cell-group>
+    </div>
     <router-view/>
   </div>
 </template>
